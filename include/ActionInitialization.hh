808b54a6645306e667107935d08501bce2439269
Carlos Vico <cvicovillalba1998@gmail.com>
2021-03-11 11:57:32 +0100

    Started ActionInitialization classes

---
+++ include/ActionInitialization.hh
@@ -0,0 +1,11 @@
+#ifndef ACTIONINITIALIZATION_HH
+#define ACTIONINITIALIZATION_HH
+#include "G4VUserActionInitialization.hh"
+
+class ActionInitialization : public G4VUserActionInitialization {
+public:
+    ActionInitialization();
+    ~ActionInitialization();
+};
+
+#endif
---
+++ src/ActionInitialization.cc
@@ -0,0 +1,1 @@
+#include "ActionInitialization.hh"
