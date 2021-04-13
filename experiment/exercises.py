87659411df3a8f71d9c76b97ad425b3de3ffdb18
Carlos <cvicovillalba1998@gmail.com>
2021-04-13 19:34:38 +0200

    Modified plotter

--- experiment/exercises.py
+++ experiment/exercises.py
@@ -1,5 +1,6 @@
 import os, re, sys
 import ROOT as r
+import numpy as np
 from copy import deepcopy
 import argparse
 import plotter as g4plt
@@ -12,11 +13,82 @@ def add_options():
     env_variables["exercise"] = parser.parse_args().exercise
     return 
 
-def set_exercise_config(exercise):
+def create_histos():
+    # Read the txt file containing this information
+    txt_name = "./experiment/g4plots.txt"
+    f = open(txt_name)
+    lines = [ re.match(env_variables["matching"], line.replace(" ", "")).groups() for line in f.readlines() if ("#" not in line[0] and line!="" and line!="\n") ]
+    hists_to_fill = {}
+    for line in lines:
+	processed_line = line[1].replace("[", "").replace("]", "").split(",") 
+	nbins = int(processed_line[0])
+	xlow  = float(processed_line[1])
+	xup   = float(processed_line[2])
+	hists_to_fill[line[0]] = r.TH1D(line[0],"", nbins, xlow, xup)
+    f.close()
+    env_variables["hists_to_fill"] = hists_to_fill
+    return 
+
+def fill_histos(var):
+    histos  = env_variables["histograms"]
+    samples = env_variables["histograms"].keys()
+    env_variables["ret_histograms"] = {}
+
+    for index, s in enumerate(samples):
+#	forCopy = deepcopy(env_variables["histograms"]["Meroli"].Clone("%s_%s_toFill"%(var, s)))
+
+	forCopy = deepcopy(env_variables["hists_to_fill"][var].Clone("%s_%s_toFill"%(var, s)))
+        h = histos[s][var] if s != "Meroli" else histos[s]
+        if s == "Meroli":
+		env_variables["ret_histograms"][s] = deepcopy(h.Clone("%s_%s"%(var, s)))
+		continue
+	for bini in range(1, h.GetNbinsX()+1):
+ 		forCopy.SetBinContent(bini, 0)
+		forCopy.Fill(h.GetBinCenter(bini), h.GetBinContent(bini))
+
+	# Store a copy
+	env_variables["ret_histograms"][s] = deepcopy(forCopy.Clone("%s_%s"%(var, s)))
+	del forCopy
+    return
+
+def run_exercise(exercise):
+    # === First of all, read the txt which has the information about the histograms that will be filled
+    create_histos()
+
     if exercise == "1":
 	env_variables["path"] = "./experiment/inputs/ex1/"
 	env_variables["outpath"] = "./experiment/results/ex1/"
+	
+    list_rfiles = [ "/".join([env_variables["path"], rfile]) for rfile in os.listdir(env_variables["path"]) if (".root" in rfile) == True ]
+    set_histograms_dict( list_rfiles if len(list_rfiles) != 1 else list_rfiles[0] )
+    env_variables["histograms"]["Meroli"] = get_histo_meroli() if exercise == "1" else None
+
+    fill_histos("Edep")
     return 
+
+def get_histo_meroli():
+  
+   # Read the .dat file from data and simulation
+   data = open("./Meroli/exp_Meroli_100MeV_electron_5p6um_Si.dat")
+   sim = open("./Meroli/Hist_Edep.dat")
+  
+   data_bins = [(float(line.split(" ")[0]), float(line.split(" ")[1].split("\n")[0])) for line in data.readlines() if "#" not in line[0] ]
+    
+   x_bins_data = [el[0]/1000 for el in data_bins]
+   x_bins_data = np.asarray(x_bins_data)
+   
+   y_bins_data = [el[1] for el in data_bins]
+   y_bins_data = np.asarray(y_bins_data)
+   
+   h_data  = r.TH1D("data_meroli", "", len(x_bins_data)-1, x_bins_data)
+   
+   for bini in range(len(x_bins_data)):
+	h_data.Fill(x_bins_data[bini], y_bins_data[bini])
+  
+   data.close()
+   sim.close()
+   return h_data
+
 
 def get_histograms(rfile):
     
@@ -33,10 +105,11 @@ def set_histograms_dict(rfiles):
     Function to store histograms from rootfiles in a 
     custom environmental variable
     '''
-    h_dict = { re.match("^(.*)/(.*).root$", rfile).groups()[1] : get_histograms(rfile) for rfile in rfiles }
-    files = list(h_dict.keys())
-    
-    env_variables["AnalysedFiles"] = files
+    if isinstance(rfiles, list):	
+	h_dict = { re.match("^(.*)/(.*).root$", rfile).groups()[1] : get_histograms(rfile) for rfile in rfiles }
+    else:
+	h_dict = {rfiles.split(".root")[0].split(env_variables["path"])[1].split("/")[1] : get_histograms(rfiles)}
+
     env_variables["histograms"] = h_dict
     return 
 
@@ -44,13 +117,9 @@ def set_histograms_dict(rfiles):
 # === Main script
 if __name__ == "__main__": 
     add_options()
-    set_exercise_config(env_variables["exercise"])    
-    # Get a list of all the rootfiles in the given path
-    list_rfiles = [ "/".join([env_variables["path"], rfile]) for rfile in os.listdir(env_variables["path"]) if (".root" in rfile) == True ]
-    # Get a dictionary with the histograms contained in the rootfiles
-    set_histograms_dict( list_rfiles if len(list_rfiles) != 1 else list_rfiles[0] )
-    
-    # Print the histograms following the txt directives.
+ 
+    run_exercise(env_variables["exercise"])
+
     g4plt.draw_histos(env_variables)
 
     os.system("cp -r ./experiment/results/ex1 ~/www/private/FPFE/")
--- experiment/plotter.py
+++ experiment/plotter.py
@@ -6,6 +6,7 @@
 import ROOT as  r
 import os, re, sys
 from copy import deepcopy
+import numpy as np
 r.gROOT.SetBatch(1)
 r.gStyle.SetOptStat(0)
 
@@ -17,7 +18,7 @@ def get_dims(opt):
         topx, topy, ww, wh = 10, 10, 1000, 800
         return ( topx, topy, ww, wh ) # last 2 for printing with 2 pads
     if opt == "legend":
-        fLegX1, fLegY1, fLegX2, fLegY2 = 0.35, 0.55, 0.89, 0.89
+        fLegX1, fLegY1, fLegX2, fLegY2 = 0.55, 0.55, 0.89, 0.89
         legendTextSize = 0.035
         return( fLegX1, fLegY1, fLegX2, fLegY2, legendTextSize )
 
@@ -39,18 +40,65 @@ def create_legend():
     return l
 
 
-def draw_histos(args):
-    if env_variables["exercise"] == 1: draw_histos_ex1(args)
+def draw_histos(env_variables):
+    if env_variables["exercise"] == "1": draw_meroli_comp(env_variables)
     return
 
-def draw_histos(env_variables):
-   if env_variables["exercise"] == "1": draw_histos_ex1(env_variables)
+def draw_meroli_comp(env_variables):
+   '''
+	Very simple function to plot the comparison with Meroli and some other plots
+   '''
+   outpath = env_variables["outpath"]
+   c = create_canvas("ex1")
+   l = create_legend()
+
+   meroli = env_variables["ret_histograms"]["Meroli"]
+   sim = env_variables["ret_histograms"]["YourApplication"]
+   
+  # meroli.Draw("hist p")
+   meroli_gr = r.TGraph(meroli)
+   sim.Draw("hist")
+   meroli_gr.Draw("l same") 
+   sim.SetLineColor(r.kRed)
+   print(sim.Integral())
+
+   sim.Scale(meroli.Integral()/sim.Integral())
+   print(meroli.Integral())
+   
+   sim.SetMarkerColor(r.kBlack)	
+#   print([sim.GetBinContent(bini) for bini in range(1, 1+sim.GetNbinsX())]) 
+
+   
+   meroli_gr.SetLineColor(r.kBlack)
+#   meroli_gr.SetLineStyle(20)
+
+   sim.SetTitle("")
+   sim.SetMinimum(0)
+   sim.GetXaxis().SetTitle("E(MeV)")
+   sim.GetYaxis().SetTitle("Events")
+   sim.GetYaxis().SetTitleOffset(1.3)
+   sim.GetYaxis().SetLabelSize(0.03)
+   sim.GetXaxis().SetLabelSize(0.03)
+ 
+   l.AddEntry(meroli, "Data", "l")
+   l.AddEntry(sim, "FPFE simulation", "l")
+ 
+   l.Draw("same")
+   draw_header()   
+   make_path(outpath)
+   c.Print(outpath + "/%s.png"%( "Meroli_comp"), 'png') 
+   c.Print(outpath + "/%s.pdf"%( "Meroli_comp" ), 'pdf')
+
    return
 
-def draw_histos_ex1(env_variables):
-   '''
-	Very simple function to plot the comparison with Meroli...
-   '''
+def draw_header():
+   H = r.TLatex()
+   H.SetTextSize(0.07)                                                    	 
+   H.DrawLatexNDC(0.1, 0.95,"#font[22]{FPFE} | #font[12]{Academic}")
+
+   return
+
+def draw_histos_ex2(env_variables):
    histos  = env_variables["histograms"]
    samples = env_variables["AnalysedFiles"] 
  
@@ -61,42 +109,43 @@ def draw_histos_ex1(env_variables):
 	      "electron": "E(MeV)" }
    
    for part in ["proton", "electron"]:	
-        c = create_canvas("edep_" + part)
-      	
-        l1 = create_legend()      
-        for index, rfile in enumerate(samples):
-	     if part not in rfile: continue
-             edep   = histos[rfile]["Edep"]
+      c = create_canvas("edep_" + part)
+      
+      l1 = create_legend()      
+      for index, rfile in enumerate(samples):
+         if part not in rfile: continue
+         edep   = histos[rfile]["Edep"]
 
-	     if (edep.GetMaximum() == 0.0): continue
- 	     thickness = float(re.match("(.*)_(.*)(um)_(.*)_(.*)", rfile).groups()[1].replace("p", "."))
-	     edep.Scale(1.0/(edep.GetMaximum()))
+         if (edep.GetMaximum() == 0.0): continue
+         thickness = float(re.match("(.*)_(.*)(um)_(.*)_(.*)", rfile).groups()[1].replace("p", "."))
+         edep.Scale(1.0/(edep.GetMaximum()))
 
-             color = index if index not in [0, 5, 10] else index+1 
-	     edep.Draw("hist same p")
-	
-	     edep.SetMaximum(1.2)
-      	     edep.SetMinimum(0)
-      	     edep.SetMarkerColor(color)
-	     edep.SetMarkerStyle(20)
-	     edep.SetMarkerSize(1)
-      	     edep.SetTitle("")
-	     
-      	     edep.GetXaxis().SetTitle(xtitles[part])
-      	     edep.GetYaxis().SetTitle("Normalized Events")
-      	     edep.GetYaxis().SetTitleOffset(1.3)
-      	     edep.GetYaxis().SetLabelSize(0.03)
-      	     edep.GetXaxis().SetLabelSize(0.03)
+         color = index if index not in [0, 5, 10] else index+1 
+         edep.Draw("hist same p")
+      
+         edep.SetMaximum(1.2)
+         edep.SetMinimum(0)
+         edep.SetMarkerColor(color)
+         edep.SetMarkerStyle(20)
+         edep.SetMarkerSize(1)
+         edep.SetTitle("")
+         
+         edep.GetXaxis().SetTitle(xtitles[part])
+         edep.GetYaxis().SetTitle("Normalized Events")
+         edep.GetYaxis().SetTitleOffset(1.3)
+         edep.GetYaxis().SetLabelSize(0.03)
+         edep.GetXaxis().SetLabelSize(0.03)
 
-	     	
-	#    inputs/ex1/silicon_107p29um_100MeV_electrons.root 
-      	     entryName = "t = %3.2f #mum"%thickness      
-      	     l1.AddEntry(edep, entryName, "p")
-     	l1.Draw("same") 
-        H = r.TLatex()
-        H.SetTextSize(0.07)                                                    	 
-        H.DrawLatexNDC(0.1, 0.92,"#font[22]{FPFE} | #font[12]{Academic}")
-        c.Print(outpath + "/%s.png"%( part ), 'png') 
-        c.Print(outpath + "/%s.pdf"%( part ), 'pdf')
+           	
+      #    inputs/ex1/silicon_107p29um_100MeV_electrons.root 
+         entryName = "t = %3.2f #mum"%thickness      
+         l1.AddEntry(edep, entryName, "p")
+      l1.Draw("same") 
+      c.Print(outpath + "/%s.png"%( part ), 'png') 
+      c.Print(outpath + "/%s.pdf"%( part ), 'pdf')
 
    return
+
+
+
+
