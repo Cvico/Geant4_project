1c04445082a2c31aa4d081ef8e20295d548cc3ee
Carlos <cvicovillalba1998@gmail.com>
2021-04-15 20:12:15 +0200

    New script for generating mac files, still pretty basic but functional2

--- scripts/generate_mac.sh
+++ scripts/generate_mac.sh
@@ -8,22 +8,39 @@
 
 # Parameters for the script
 
-THICKNESS=$1
-ENERGY=$2
-PARTICLE=$3
-OUTPATH=$4
+MATERIAL=$1
+THICKNESS=$2
+ENERGY=$3
+PARTICLE=$4
+OUTPATH=$5
 
 TEMPLATE="template.mac"
 
+# Some naming conventions
+
+if [[ $MATERIAL == "silicon" ]]; then MATERIALNAME="G4_Si"; fi
+if [[ $MATERIAL == "argon" ]]; then MATERIALNAME="G4_Ar"; fi
+if [[ $MATERIAL == "aluminum" ]]; then MATERIALNAME="G4_Al"; fi
+if [[ $MATERIAL == "iron" ]]; then MATERIALNAME="G4_Fe"; fi
+if [[ $MATERIAL == "lAr" ]]; then MATERIALNAME="G4_lAr"; fi
+if [[ $MATERIAL == "lead" ]]; then MATERIALNAME="G4_Pb"; fi
+
+if [[ $PARTICLE == "e" ]];then PARTICLENAME="electron"; fi
+if [[ $PARTICLE == "proton" ]];then PARTICLENAME="proton"; fi
+if [[ $PARTICLE == "mu" ]];then PARTICLENAME="muon"; fi
 
 # Extract the energy value and the unit from the input
 ENERGYVAL=$(echo $ENERGY | awk -F'_' '{print $1}')
 UNIT=$(echo $ENERGY | awk -F'_' '{print $2}')
-# Convert the thickness to a string of the type: 5.6-->5p6 so it can be used in the name of the output mac file
-THICKNESSTRING=$(echo $THICKNESS | sed -e 's/\./p/')
 
 
-FILENAME="silicon_"$THICKNESSTRING"um_"$ENERGYVAL$UNIT"_"$PARTICLE.mac
+# Convert the thickness to a string of the type: 5.6_unit -->5p6unit so it can be used in the name of the output mac file
+THICKNESSVAL=$(echo $THICKNESS | awk -F'_' '{print $1}')
+THICKNESSUNIT=$(echo $THICKNESS | awk -F'_' '{print $2}')
+THICKNESSTRING=$(echo $THICKNESSVAL | sed -e 's/\./p/')
+
+
+FILENAME=$MATERIAL"_"$THICKNESSTRING$THICKNESSUNIT"_"$ENERGYVAL$UNIT"_"$PARTICLENAME.mac
 
 echo $FILENAME
 
@@ -33,13 +50,19 @@ cp $TEMPLATE $FILENAME
 
 
 # Now edit the file
+# Change material
+cat $FILENAME | sed -e "s/Silicon/$MATERIAL/" > temp.mac | mv temp.mac $FILENAME
+cat $FILENAME | sed -e "s/G4_Si/$MATERIALNAME/" > temp.mac | mv temp.mac $FILENAME
+
 # Change thickness
-cat $FILENAME | sed -e "s/5.6/$THICKNESS/" > temp.mac | mv temp.mac $FILENAME
+cat $FILENAME | sed -e "s/5.6 um/$THICKNESSVAL $THICKNESSUNIT/" > temp.mac | mv temp.mac $FILENAME
 
 # Change Energy of the gun of particles
 cat $FILENAME | sed -e "s/100 MeV/$ENERGYVAL $UNIT/" > temp.mac | mv temp.mac $FILENAME
 
-# Change the Particle that is being studied
+
+
+## Change the Particle that is being studied
 cat $FILENAME | sed -e "s/e-/$PARTICLE-/" > temp.mac | mv temp.mac $FILENAME
 
 
